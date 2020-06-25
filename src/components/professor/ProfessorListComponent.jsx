9709c32f632756db52600461744f3516e1f8ceb9
Stilyan Stoyanov <stilyan.stoyanov@softwaregroup.com>
2020-06-25 18:29:25 +0300

    feat: create ability to save professor's phone and email at the clipboard

--- src/components/professor/ProfessorListComponent.jsx
+++ src/components/professor/ProfessorListComponent.jsx
@@ -36,10 +36,8 @@ class ProfessorListComponent extends Component {
     }
 
     getMyProfessors(student){
-        console.log("I will show you my professors")
         ProfessorService.getProffesorsBySpecialtyId(student.specialtyId, student.semester)
         .then(response => {
-        console.log(response.data)
         this.setState(
             {
                 professors: response.data
@@ -52,10 +50,8 @@ class ProfessorListComponent extends Component {
     }
 
     getAllProfessors(){
-        console.log("I will show you all professors")
         ProfessorService.getAllProfessors()
         .then(response => {
-        console.log(response.data)
         this.setState(
             {
                 professors: response.data
@@ -76,7 +72,7 @@ class ProfessorListComponent extends Component {
                         <div className="row justify-content-md-center">
                             {
                                 this.state.professors.map(
-                                    professor =>                                    
+                                    professor =>      
                                     <div className="col-md-3 col-xs-1">
                                         <div className="card profile-card-3">
                                             <div className="background-block">
@@ -86,10 +82,25 @@ class ProfessorListComponent extends Component {
                                                 <img src={professorImage} alt="profile" className="profile"/>
                                             </div>
                                             <div className="card-content">
-                                                <h2>{professor.fullName}<small>{professor.cabinet}</small></h2>
+                                                <h2>{professor.fullName.split(" ")[0]}</h2>
+                                                <h2>{professor.fullName.split(" ")[1]}<small>{professor.cabinet}</small></h2>
                                                 <div className="icon-block">
-                                                    <i className="fa fa-phone"></i>
-                                                    <i className="fa fa-envelope"></i>
+                                                    <button className="buttonPhone" onClick={() => { 
+                                                        var phone = professor.phoneNumber
+                                                        var name = professor.fullName
+                                                        navigator.clipboard.writeText(phone)
+                                                        alert("Телефонният номер на " + name + " е копиран във вашият Clipboard");
+                                                    }}>
+                                                        <i className="fa fa-phone"></i>
+                                                    </button>
+                                                    <button className="buttonMail" onClick={() => { 
+                                                        var email = professor.email
+                                                        var name = professor.fullName
+                                                        navigator.clipboard.writeText(email)
+                                                        alert("Имейлът " + name + " е копиран във вашият Clipboard");
+                                                    }}>
+                                                        <i className="fa fa-envelope"></i>
+                                                    </button>
                                                     <p id="professorPhone">{professor.phoneNumber}</p>
                                                     <p id="professorMail">{professor.email}</p>
                                                 </div>
@@ -106,7 +117,7 @@ class ProfessorListComponent extends Component {
             </>
         )
     }
-
 }
+
 
 export default ProfessorListComponent;
