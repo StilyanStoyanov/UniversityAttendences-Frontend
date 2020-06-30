328b0719e5b92cf2b285e51cfa36716155d747c9
Stilyan Stoyanov <stilyan.stoyanov@softwaregroup.com>
2020-06-30 20:04:13 +0300

    feat: visualize student's attendances

--- src/components/student/StudentAttendances.jsx
+++ src/components/student/StudentAttendances.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import AttendanceService from '../../service/AttendanceService.js'
+import './StudentAttendances.css'
 
 class StudentAttendances extends Component {
 
@@ -9,8 +10,9 @@ class StudentAttendances extends Component {
         this.state = {
             attendances: []
         }
+    
+        this.handleClick = this.handleClick.bind(this);
     }
-
     
     componentDidMount(){
         this.refreshAttendances();
@@ -28,6 +30,45 @@ class StudentAttendances extends Component {
             error => console.log(error.response.message)
         )
     }
+
+    handleClick(attendance) {
+        let updatedList = this.state.attendances.map(obj => {
+           if(obj.id === attendance.id) {
+             return Object.assign({}, obj, {
+                hidden:!attendance.hidden
+             });
+           }
+           return obj;
+        });
+        this.setState({
+          attendances : updatedList
+        });
+    }
+
+    renderAttendances(attendance){
+
+        const iconsMap = 
+            { 
+                "YES": "check",
+                "NO": "circle-o"
+            };
+        
+        return(
+            <div className="row justify-content-md-center" id="expanded-row">
+                {
+                    Object.entries(attendance.attendances).map(
+                    ([key, value]) => 
+                        <div className="col-1" key={key}>
+                            <p className="innerKey">У-{key}</p>
+                            <p className="innerValue">  
+                                <i className={`fa fa-${iconsMap[{value}.value]}`} aria-hidden="true"></i>
+                            </p>
+                        </div>
+                    )
+                }
+            </div>
+        )
+    }
     
     render(){
         return(
@@ -35,23 +76,28 @@ class StudentAttendances extends Component {
             {
                 this.state.attendances.map(
                     attendance => 
-                    <div className = "table-responsive col-md-12" key = {attendance.id}>
-                        <table className = "table" >
-                            <thead>
-                                <tr>
-                                    <th>Студент:</th>
-                                        <td>{attendance.studentName}</td>
-                                    <th >Предмет:</th>
-                                        <td>{attendance.subjectName}</td>
-                                    <th>Семестър:</th>
-                                        <td>{attendance.semester}</td>
-                                    <th>Присъствия:</th>
-                                        <td>{attendance.countOfAttendances}</td>
-                                </tr>
-                            </thead>                
-                        </table>
+                    <div className = "attendances" key = {attendance.id}>
+                        <div className="row justify-content-center">
+                            <div className="col-4">
+                                {attendance.subjectName}
+                            </div>
+                            <div className="col-2">
+                                Семестър: {attendance.semester}
+                            </div>
+                            <div className="col-3">
+                                Необходим брой присъствия: {attendance.countOfAttendances.length}
+                            </div>
+                            <div className="col-3">
+                                <button 
+                                type="button" 
+                                className="btn btn-primary"
+                                onClick={() => this.handleClick(attendance)}
+                                >Провери присъствия</button>
+                            </div>
+                            {!attendance.hidden && this.renderAttendances(attendance)}
+                        </div>
                     </div>
-                )
+                )               
             }
             </>
         )
